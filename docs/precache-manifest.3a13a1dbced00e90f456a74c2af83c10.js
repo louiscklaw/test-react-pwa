a2077ee3e795f9377105fc842f077994b942a6f0
louiscklaw <louiscklaw@gmail.com>
2020-04-01 17:32:51 +0800

    update to default react PWA,

---
+++ docs/precache-manifest.3a13a1dbced00e90f456a74c2af83c10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f659b407178b203a5aec15166276d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3edf861c4c6526d46840",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbe10626080be1fe9466",
+    "url": "/static/js/2.ddc2d1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddc2d1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edf861c4c6526d46840",
+    "url": "/static/js/main.4e6e152f.chunk.js"
+  },
+  {
+    "revision": "732368abfa4169e145d8",
+    "url": "/static/js/runtime-main.467e43d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
